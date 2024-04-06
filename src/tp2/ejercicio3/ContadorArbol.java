35d1424ab0572ee22c7b2c70849d3be14e0c2b37
fabioo66 <torrejonfabio@gmail.com>
2024-04-06 15:46:33 -0300

    AYED

--- src/tp2/ejercicio3/ContadorArbol.java
+++ src/tp2/ejercicio3/ContadorArbol.java
@@ -1,7 +1,6 @@
 package tp2.ejercicio3;
 
 import java.util.ArrayList;
-
 import tp2.ejercicio2.BinaryTree;
 
 public class ContadorArbol {
@@ -11,11 +10,17 @@ public class ContadorArbol {
 		this.ab = ab;
 	}
 	
-	public ArrayList<Integer> numerosPares(){
+	public ArrayList<Integer> numerosParesA(){
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		this.inOrden(ab, result);
 		return result;
 	}
+	
+	public ArrayList<Integer> numerosParesB(){
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		this.postOrden(ab, result);
+		return result;
+	}	
 	
 	private void inOrden(BinaryTree<Integer> ab, ArrayList<Integer> lista) {
 		if (ab.hasLeftChild()) {
@@ -39,5 +44,25 @@ public class ContadorArbol {
 		if (ab.hasRightChild()) {
 			this.postOrden(ab.getRightChild(), lista);
 		}
+	}
+	
+	public static void main(String[] args) {
+		BinaryTree<Integer> ab = new BinaryTree<Integer>(40);
+		BinaryTree<Integer> hijoIzquierdo = new BinaryTree<Integer>(25);
+		BinaryTree<Integer> hijoDerecho = new BinaryTree<Integer>(78);
+		hijoIzquierdo.addLeftChild(new BinaryTree<Integer>(10));
+		hijoIzquierdo.addRightChild(new BinaryTree<Integer>(32));
+		ab.addLeftChild(hijoIzquierdo);
+		ab.addRightChild(hijoDerecho);
+		
+		ContadorArbol contador = new ContadorArbol(ab);
+		
+		ArrayList<Integer> lista = contador.numerosParesA();
+		
+		System.out.println("Imprimo lista de numeros pares");
+		for(int i : lista) {
+			System.out.println(i);
+		}
+		
 	}
 }
