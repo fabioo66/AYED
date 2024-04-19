128b6c1153baab7d91d97ef8bc6e8f788792dc7e
fabioo66 <torrejonfabio@gmail.com>
2024-04-19 19:48:36 -0300

    AYED

--- src/tp2/ejercicio9/ParcialArboles.java
+++ src/tp2/ejercicio9/ParcialArboles.java
@@ -33,25 +33,25 @@ public class ParcialArboles {
 	}
 	
 	public static void main(String[] args) {
-		 System.out.println("Test Ejercicio9");
-		 ParcialArboles test = new ParcialArboles();
+		System.out.println("Test Ejercicio9");
+		ParcialArboles test = new ParcialArboles();
 	        
-	     BinaryTree<Integer> ab = new BinaryTree<Integer>(20);
-	     ab.addLeftChild(new BinaryTree<Integer>(5));
-	     ab.getLeftChild().addLeftChild(new BinaryTree<Integer>(-5));
-	     ab.getLeftChild().addRightChild(new BinaryTree<Integer>(10));
-	     ab.getLeftChild().getRightChild().addLeftChild(new BinaryTree<Integer>(1));
-         ab.addRightChild(new BinaryTree<Integer>(30));
-         ab.getRightChild().addLeftChild(new BinaryTree<Integer>(50));
-         ab.getRightChild().addRightChild(new BinaryTree<Integer>(-9));
-	     ab.getRightChild().getLeftChild().addRightChild(new BinaryTree<Integer>(4));
-	     ab.getRightChild().getLeftChild().getRightChild().addRightChild(new BinaryTree<Integer>(6));   
+		BinaryTree<Integer> ab = new BinaryTree<Integer>(20);
+		ab.addLeftChild(new BinaryTree<Integer>(5));
+		ab.getLeftChild().addLeftChild(new BinaryTree<Integer>(-5));
+		ab.getLeftChild().addRightChild(new BinaryTree<Integer>(10));
+		ab.getLeftChild().getRightChild().addLeftChild(new BinaryTree<Integer>(1));
+		ab.addRightChild(new BinaryTree<Integer>(30));
+		ab.getRightChild().addLeftChild(new BinaryTree<Integer>(50));
+		ab.getRightChild().addRightChild(new BinaryTree<Integer>(-9));
+		ab.getRightChild().getLeftChild().addRightChild(new BinaryTree<Integer>(4));
+		ab.getRightChild().getLeftChild().getRightChild().addRightChild(new BinaryTree<Integer>(6));   
 	     
-	     ab.printLevelTraversal();
-	     System.out.println("Nuevo arbol:");
+		ab.printLevelTraversal();
+		System.out.println("Nuevo arbol:");
+	    
+		BinaryTree<Data> arbolNuevo = test.sumAndDif(ab);
 	     
-	     BinaryTree<Data> arbolNuevo = test.sumAndDif(ab);
-	     
-	     arbolNuevo.printLevelTraversal();
+		arbolNuevo.printLevelTraversal();
 	}
 }
