45321d62be3a9513120bc51518e30f4e457b7e04
fabioo66 <torrejonfabio@gmail.com>
2024-04-19 12:31:33 -0300

    AYED

---
+++ src/tp2/ejercicio7/ParcialArboles.java
@@ -0,0 +1,87 @@
+package tp2.ejercicio7;
+
+import tp2.ejercicio2.BinaryTree;
+
+public class ParcialArboles {
+	BinaryTree <Integer> ab;
+	
+	public ParcialArboles (BinaryTree <Integer> ab) {
+		this.ab = ab;
+	}
+	
+	public boolean isLeftTree (int num) {
+		boolean cumple = false;
+		if(ab != null) {
+			BinaryTree<Integer>subArbol = buscarArbol(ab, num);
+			if(subArbol != null) cumple = procesarSubArbol(subArbol);
+		}
+		return cumple;
+	}
+	
+	private BinaryTree<Integer> buscarArbol(BinaryTree<Integer> ab, int num){
+		if(ab.getData() == num) return ab;
+		BinaryTree <Integer> arbolNuevo = null;
+		if (ab.hasLeftChild()) {
+			arbolNuevo = buscarArbol(ab.getLeftChild(), num);
+		}
+		if (ab.hasRightChild() && arbolNuevo == null) {
+			arbolNuevo = buscarArbol(ab.getRightChild(), num);
+		}
+		return arbolNuevo;
+	}
+	
+	 private boolean procesarSubArbol(BinaryTree<Integer> ab) {
+		 int izq,der;
+		 if (ab.hasLeftChild())
+			 izq = contarHijosUnicos(ab.getLeftChild());
+	     else
+	    	 izq = -1;
+	     if (ab.hasRightChild())
+	         der = contarHijosUnicos(ab.getRightChild());
+	     else
+	         der = -1;
+	     System.out.println("izquierda " + izq + " derecha " + der);
+	     
+	     return izq > der;
+	 }
+	 
+	 private int contarHijosUnicos(BinaryTree<Integer> ab) {
+		 int cantHijos = 0;
+		 
+		 if (ab.hasLeftChild()) cantHijos += contarHijosUnicos(ab.getLeftChild());
+		 
+	     if (ab.hasRightChild()) cantHijos += contarHijosUnicos(ab.getRightChild());
+
+	     if ((ab.hasLeftChild() && !ab.hasRightChild()) || (!ab.hasLeftChild() && ab.hasRightChild()))
+	    	 cantHijos++;
+	     
+	     return cantHijos;
+	  }
+	
+	public static void main(String[] args) {
+		BinaryTree<Integer> ab = new BinaryTree<Integer>(1);
+		BinaryTree<Integer> hijoIzquierdo = new BinaryTree<Integer>(4);
+		BinaryTree<Integer> hijoDerecho = new BinaryTree<Integer>(7);
+		BinaryTree<Integer> TreintaDos = new BinaryTree<Integer>(10);
+		
+		hijoIzquierdo.addLeftChild(new BinaryTree<Integer>(2));
+		hijoIzquierdo.addRightChild(TreintaDos);
+		TreintaDos.addRightChild(new BinaryTree <Integer>(9));
+		
+		hijoDerecho.addLeftChild(new BinaryTree<Integer>(8));
+		hijoDerecho.addRightChild(new BinaryTree<Integer>(12));
+		
+		ab.addLeftChild(hijoIzquierdo);
+		ab.addRightChild(hijoDerecho);
+		
+		ParcialArboles arbol = new ParcialArboles(ab);
+		
+		BinaryTree<Integer> nuevoArbol = arbol.buscarArbol(ab, 10);
+		
+		nuevoArbol.printLevelTraversal();
+		
+		System.out.println("----------------------");
+		
+		System.out.println(arbol.isLeftTree(7));
+	}
+}
