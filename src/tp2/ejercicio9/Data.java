e6af4e38fc13fea61a9628110b7faa72b50874f7
fabioo66 <torrejonfabio@gmail.com>
2024-04-19 16:01:21 -0300

    incompleto

---
+++ src/tp2/ejercicio9/Data.java
@@ -0,0 +1,24 @@
+package tp2.ejercicio9;
+
+public class Data {
+	private int suma;
+	private int diferencia;
+	
+	public Data(int suma, int diferencia) {
+		super();
+		this.suma = suma;
+		this.diferencia = diferencia;
+	}
+	public int getSuma() {
+		return suma;
+	}
+	public void setSuma(int suma) {
+		this.suma = suma;
+	}
+	public int getDiferencia() {
+		return diferencia;
+	}
+	public void setDiferencia(int diferencia) {
+		this.diferencia = diferencia;
+	}
+}
---
+++ src/tp2/ejercicio9/ParcialArboles.java
@@ -0,0 +1,22 @@
+package tp2.ejercicio9;
+
+import tp2.ejercicio2.BinaryTree;
+
+public class ParcialArboles {
+	public BinaryTree<Data> sumAndDif(BinaryTree<Integer> arbol){
+		
+		BinaryTree<Integer> izq = null;
+		BinaryTree<Integer> der = null;
+		
+		if (arbol.hasLeftChild()) {
+			izq = 
+		}
+		if (arbol.hasRightChild()) {
+			
+		}
+		BinaryTree <Data> arbolData = new BinaryTree<Data>(); 
+		arbolData.addLeftChild(izq);
+		arbolData.addRightChild(der);
+		return arbolData;
+	}
+}
